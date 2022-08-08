5f910fc991718f04aafbbb60d51b07cc1fa9a4e6
LucasBerta <lucasberta59@gmail.com>
2022-08-08 16:11:07 +0100

    skeleton

--- src/features/BomCalc.js
+++ src/features/BomCalc.js
@@ -1,11 +1,36 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './BomCalc.css';
+
+const USED_FIELDS = ['No.', 'Description', 'Quantity', 'Unit Price Excl. VAT', 'Discounted Unit Price', 'Line Amount Excl. VAT'];
 
 function BomCalc() {
     const [rawData, setRawData] = useState('');
+    const [discount, setDiscount] = useState('');
+    const [indexes, setIndexes] = useState([]);
 
-    function handleOnRawDataChange(event) {
-        setRawData(event.target.value);
+    useEffect(() => {
+        setUpIndexes();
+    }, [rawData]);
+
+    function setUpIndexes() {
+        const heads = getRows()[0].split(/\t+/g);
+        let _indexes = [];
+        heads.forEach((head, index) => {
+            if (!!USED_FIELDS.some(item => item === head)) {
+                _indexes.push(index);
+            }
+        });
+        setIndexes(_indexes);
+    }
+
+    function pasteData() {
+        navigator.clipboard.readText().then(text => {
+            setRawData(text);
+            setDiscount('');
+        }).catch(err => {
+            setRawData('');
+            alert(err);
+        });
     }
 
     function getRows() {
@@ -13,15 +38,19 @@ function BomCalc() {
     }
 
     function getTableHeads() {
+        if (indexes.length === 0) return [];
+
         const heads = getRows()[0].split(/\t+/g);
-        return heads;
+        return heads.filter((_, index) => indexes.some(i => i === index));
     }
 
     function getBodyRows() {
+        if (indexes.length === 0) return [];
+
         const bodyRows = getRows().slice(1);
         const tableHeads = getTableHeads();
-        return bodyRows.map((row, index) => {
-            const columns = row.split(/\t/g);
+        return bodyRows.map(row => {
+            const columns = row.split(/\t/g).filter((_, index) => indexes.some(i => i === index));
             let columnsObj = {};
             tableHeads.forEach((head, index) => {
                 columnsObj[head] = columns[index];
@@ -31,17 +60,30 @@ function BomCalc() {
     }
 
     function getTotalValue() {
+        if (getBodyRows().length === 0) return 0;
         const itemPrices = getBodyRows().reduce((prev, curr) => {
             const value = parseFloat(curr['Unit Price Excl. VAT'].replace(',', ''));
-            const v = Number.isNaN(value) ? 0 : value;
+            const qty = parseInt(curr['Quantity']);
+            const v = Number.isNaN(value) ? 0 : value * qty;
             return prev + v;
         }, 0);
         return itemPrices;
     }
 
+    function calculateDiscount(event) {
+        const poPrice = event.target.value;
+        const totalValue = getTotalValue();
+        const discount = 100 * (totalValue - poPrice) / totalValue;
+        setDiscount(discount.toFixed(5));
+    }
+
+    function copyDiscount() {
+        navigator.clipboard.writeText(discount);
+    }
+
     return (
         <div className='bom-calc'>
-            <textarea id='raw-data' placeholder='Paste NAV lines here' value={rawData} onChange={handleOnRawDataChange} />
+            <button onClick={pasteData}>Paste data</button>
 
             <div className='table-content'>
                 <table className='bom-calc-table'>
@@ -62,9 +104,12 @@ function BomCalc() {
                         ))}
                     </tbody>
                 </table>
-                <div>
-                    <span className='bom-total-value'>Total Value: {getTotalValue().toFixed(2)}</span>
-                </div>
+            </div>
+            <div className='bom-total-value-container'>
+                <span className='bom-total-value'>Total Value: <span>{getTotalValue().toFixed(2)}</span></span>
+                <input id='po-price' placeholder='PO Price' type='number' onChange={calculateDiscount} />
+                <span className='discount'>Discount: <span>{discount}</span></span>
+                <button onClick={copyDiscount}>Copy</button>
             </div>
         </div>
     );
